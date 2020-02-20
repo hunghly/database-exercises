67b8fd81f04c9f7cee530e5d46d8e38b7c604c22
Hung Ly <hunghly@gmail.com>
2020-02-20 15:43:36 -0600

    finished indexes

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -3,3 +3,9 @@ USE employees;
 SELECT CONCAT(emp_no, ' - ', last_name, ' ', first_name) AS full_name, birth_date AS DOB
 FROM employees
 LIMIT 10;
+
+SELECT emp.first_name, emp.last_name, title.title, sal.salary
+FROM employees AS emp
+JOIN titles AS title ON emp.emp_no = title.emp_no
+JOIN salaries AS sal ON emp.emp_no = sal.emp_no
+ORDER BY salary DESC;
